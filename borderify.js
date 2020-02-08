d3fde74023349a03511abd7141096314047bb57e
papiamento <1024836+papiamento@users.noreply.github.com>
2020-02-08 13:21:52 -0500

    simple example on how we can add something to the DOM tree, catch the current url and reirect to a different page

--- borderify.js
+++ borderify.js
@@ -1,1 +1,17 @@
 document.body.style.border = "5px solid red";
+
+
+// if url is cnn, then redirect
+
+var url = document.URL;
+
+console.log(`url: ${url}`);
+
+const firstDiv = document.querySelector('div');
+
+let textNode = document.createTextNode(url);
+
+firstDiv.appendChild(textNode);
+
+
+window.location = 'https://www.cnn.com';
